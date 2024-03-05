eb4def6fabf390dfa86931d1af80423161814eb6
NemanjaVukosavljevic <125715780+NemanjaVukosavljevic@users.noreply.github.com>
2024-03-05 20:04:30 +0100

    removed aps

--- src/App.js
+++ src/App.js
@@ -22,24 +22,6 @@ function App() {
           </Routes>
         </div>
       </Router>
-
-      {/* Domaći zadatak: SpaceX informacioni sistem
-      Cilj domaćeg zadatka je da završite aplikaciju koja prikazuje informacije o svim
-      SpaceX lansiranjima,letelicama i raketama, kao i o samoj kompaniji. Dodati da
-      aplikacija ima listu svih letelica i raketa, kao i pojedinačne stranice za prikaz
-      detalja o svakoj letelici i raketama.
-      Korak 1: Implementirajte komponentu ShipsView i RocketsView.
-      Ažurirajte komponente ShipsView i RocketsView
-      Fetch-ujte podatke o brodovima i raketama.
-      Prikazite ih na odgovarajućim stranicama.
-      Dodajte Loading, Error i State fazu prikaza za obe komponente.
-      Neka svaki item iz vaše liste vodi na pojedinačnu rutu sa id-jem.
-      Korak 2: Kreirajte pojedinačne stranice za rakete i brodove.
-      Napravite komponente SingleShipView i SingleRocketView.
-      Koristite parametre za identifikaciju i fetch podataka o određenoj raketi ili
-      brodu.
-      Implementirajte Loading, Error i State fazu prikaza.
-      Sloboda je na vama da li će te koristiti lokalni state, ili Context ili Reducer. */}
     </div>
   );
 }
